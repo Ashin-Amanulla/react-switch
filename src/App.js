0f131a24c00fc58275f783b394885afe4a4f9770
ashin <ashin.jnv@gmail.com>
2022-01-17 11:09:23 +0530

    bb

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+//components
+import Lamp from './Lamp';
+import LightSwitch from './LightSwitch';
+
+
+const Room = styled.div`
+position: relative;
+width: 500px;
+height: 500px;
+border: 10px solid black;
+margin: 0 auto;
+`;
+
 
 function App() {
+
+  //* useState() Hook is the initial state. It returns a pair of values: the current state and a function that updates it.  
+  const [isLampOneOn, setLampOneOn] = useState(true); //LampOne
+  const [isLampTwoOn, setLampTwoOn] = useState(false); //LampTwo
+
+  //* below functions toggle the useState(boolean) value
+  const switchLampOne = () => setLampOneOn((prev => !prev)); //switch of lamp one
+  const switchLampTwo = () => setLampTwoOn((prev => !prev)); //switch of lamp two
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Room>
+
+      <Lamp lampOn={isLampOneOn} position='left' />  {/* lamp1  */}
+      <Lamp lampOn={isLampTwoOn} position='right' />  {/* lamp2  */}
+
+      {/* switchOne  */}
+      <LightSwitch    
+        name='one'
+        callback={switchLampOne}
+        switchOn={isLampOneOn}
+        position='left'
+      />
+
+      {/* switchTwo  */}
+      <LightSwitch
+        name='two'
+        callback={switchLampTwo}
+        switchOn={isLampTwoOn}
+        position='right'
+      />
+    </Room>
   );
 }
 
